a1040b858e366897848a8e1339176605c7fcf217
islom <alixujaevislom@gmail.com>
2024-06-06 19:23:09 +0500

    done

--- index.ts
+++ index.ts
@@ -1,9 +1,26 @@
 const http = require('http');
 
-const server = http.createServer((req, res) => {
-  console.log(req.url);
-  res.write('<h1>Hello World!</h1>');
-  res.end()
+const server = http.createServer((req: any, res:any) => {
+  if(req.method === "GET"){
+    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+    res.end(`
+      <h1>Send your name</h1>
+
+      <form method="post" action="/">
+        <input type="text" name="name">
+        <button type="submit">Send</button>
+      </form>
+    `)
+  }else if(req.method === "POST"){
+
+    req.on('data', (data:any) => {
+      console.log(Buffer.from(data).toString('utf-8').split("=")[1]);
+      
+    });
+
+    req.end(`
+      <h1>Hello, ${Buffer.from(req.body).toString('utf-8').split("=")[1]}</h1>`);
+  }
 });
 
 
